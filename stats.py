d8698a3e35a58d813d4d6d4df9981ffa20faba08
arkwave <ananthravikumar@gmail.com>
2016-03-07 17:02:43 -0800

    changed implementation of median

--- stats.py
+++ stats.py
@@ -1,13 +1,10 @@
 """
 This is our brand new stats module
 """
-
+import numpy as np
 def mean(x):
     raise NotImplementedError()
 
 def median(data):
 	#finds the median value of a given range of values.
-	data.sort()
-	index = len(data)
-	return data(index//2)
-	
+	return np.median(data)
