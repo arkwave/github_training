4e9864c77b5c432736c142c7c8631c6a02fe084e
arkwave <ananthravikumar@gmail.com>
2016-03-07 16:43:43 -0800

    added median function

--- stats.py
+++ stats.py
@@ -4,3 +4,10 @@ This is our brand new stats module
 
 def mean(x):
     raise NotImplementedError()
+
+def median(data):
+	#finds the median value of a given range of values.
+	data.sort()
+	index = len(data)
+	return data(index//2)
+	
